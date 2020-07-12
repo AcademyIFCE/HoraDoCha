9253d1727249e7b1bdf28a8abae7cabe821649ed
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-07-12 14:18:12 -0300

    Criacao das classes de testes

---
+++ HoraDoChaTests/TearTimeModelTests.swift
@@ -0,0 +1,14 @@
+//
+//  TearTimeModelTests.swift
+//  HoraDoChaTests
+//
+//  Created by Ronaldo Gomes on 12/07/20.
+//  Copyright © 2020 Gabriela Bezerra. All rights reserved.
+//
+
+import XCTest
+@testable import HoraDoCha
+
+class TearTimeModelTests: XCTestCase {
+
+}
