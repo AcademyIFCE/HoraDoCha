e1cfb7740b31eb0a4e2d0ba5610c036ab65ed58b
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-07-13 17:44:03 -0300

    Testes da claase PreferencesModel

--- HoraDoChaTests/PreferencesModelTests.swift
+++ HoraDoChaTests/PreferencesModelTests.swift
@@ -10,5 +10,41 @@ import XCTest
 @testable import HoraDoCha
 
 class PreferencesModelTests: XCTestCase {
-
+    
+    func test_selectSound_setSino_notChaleira(){
+        
+        //Given
+        var sut = PreferencesModel()
+        
+        //When
+        sut.selectedSound = "Sino"
+        
+        //Then
+        XCTAssertNotEqual(sut.selectedSound, "Chaleira")
+    }
+    
+    func test_selectedCup_setTea2_notTea1(){
+        
+        //Given
+        var sut = PreferencesModel()
+        
+        //When
+        sut.selectedCup = "tea2"
+        
+        //Then
+        XCTAssertNotEqual(sut.selectedCup, "tea1")
+    }
+    
+    func test_selectedSoundIndex_indexOfGotadeAgua_indexTwo(){
+        
+        //Given
+        var sut = PreferencesModel()
+        sut.selectedSound = "Gota de Água"
+        
+        //When
+        let indexOfSound = sut.selectedSoundIndex
+        
+        //Then
+        XCTAssertEqual(indexOfSound, 2)
+    }
 }
